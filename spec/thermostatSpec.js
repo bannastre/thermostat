c9875ce0b1fb3bee7236ae018853c24a62cd5948
bannastre <bannastre@gmail.com>
2017-06-07 12:09:26 +0100

    Increase temperature by any number of degrees - 'Up' function defined as increaseTemperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -10,4 +10,9 @@ describe('Thermostat', function(){
   it('starts at 20 degrees', function(){
     expect(thermostat.temperature).toEqual(20)
   });
+
+  it("increases the temperature with an 'Up' function", function(){
+    thermostat.increaseTemperature(1);
+    expect(thermostat.temperature).toEqual(21)
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,3 +3,7 @@
 function Thermostat(){
   this.temperature = 20
 };
+
+Thermostat.prototype.increaseTemperature = function(degrees) {
+  this.temperature += degrees
+};
