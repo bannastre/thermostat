1a18c9c2500ecf6611ebfe9613111472cef44c0c
bannastre <bannastre@gmail.com>
2017-06-08 11:03:33 +0100

    Add all JQuery event listeners

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
-
   $('#temperature').text(thermostat.currentTemperature);
 
   $('#temperature-up').on('click', function() {
@@ -12,4 +11,27 @@ $(document).ready(function(){
     thermostat.decreaseTemperature();
     $('#temperature').text(thermostat.currentTemperature);
   });
+
+  $('#temperature-reset').on('click', function(){
+    thermostat.reset();
+    $('#temperature').text(thermostat.currentTemperature);
+  });
+
+  $('#power-saving-status').on('click', function(){
+    thermostat.isPowerSavingModeOn();
+    $('#temperature').text(thermostat.currentTemperature);
+  });
+
+  $('#powersaving-on').on('click', function(){
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on');
+    $('#temperature').text(thermostat.currentTemperature);
+  });
+
+  $('#powersaving-off').on('click', function(){
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off');
+    $('#temperature').text(thermostat.currentTemperature);
+  });
+
 });
